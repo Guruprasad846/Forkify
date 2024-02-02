df44340b94bbad3c401d6d84a5c1132ee82b57f1
Guruprasad846 <guruprasadkulkarni846@gmail.com>
2024-02-02 21:54:34 +0530

    change

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '87c69a6f-e169-4997-b621-858e322a2109';
+export const KEY = 'f3e707c4-8165-431c-8f31-ee8b296d04c4';
 export const MODAL_CLOSE_SEC = 2.5;
